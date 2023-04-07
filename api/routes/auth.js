eb911da2169f4d14db516fc999715f0057165c7b
almarzouk <jumaa.almarzouk@gmail.com>
2023-04-07 15:58:59 +0200

    api

---
+++ api/routes/auth.js
@@ -0,0 +1,35 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+// REGISTER
+router.post("/register", async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashpass = await bcrypt.hash(req.body.password, salt);
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashpass,
+    });
+    const user = await newUser.save();
+    const { password, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// Login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(400).json("Wrong Credentials");
+    const validate = await bcrypt.compare(req.body.password, user.password);
+    !validate && res.status(400).json("Wrong Credentials");
+    const { password, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+module.exports = router;
